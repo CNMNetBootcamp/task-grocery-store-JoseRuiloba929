c4a007989944bbe2c1090ab839ac7f9ea6b285f9
Jose Ruiloba <jose_ruiloba@outlook.com>
2019-01-23 12:31:22 -0700

    Revised Final

---
+++ Task3/Pseudocode.cs
@@ -0,0 +1,24 @@
+/*### Grocery Store ###
+A grocery store collects bottles for recycling. Write a program that takes in the number of bottles received each day and outputs the weekly total and the amount paid. 
+(A bottle is worth 10 cents)
+You must use a loop.
+You must accumulate the number of bottles in the loop
+YOu do not need to care about the day of the week */
+
+//setup
+int dailyTotBottles = 0;
+int day = 1;
+single Amountpaid = 0;
+single wklytotBottles = 0;
+single wklytotPaid = 0;
+//input
+loop for each day entry
+"Enter the total bottles for day"
+Get dailyTotBottles;
+
+//process
+wklytotPaid= totbottles*.10f
+
+//output
+"The total number bottles for the week is " + wklytotBottles + "the ammont paid is" + wklytotPaid
+
--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 
+
+
 namespace Task3
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //setup
+
+            int dailyTotBottles = 0;
+            Single wklytotBottles = 0;
+            Double wklytotPaid = 0;
+            string validateEntry = string.Empty;
+            bool IsString = true;
+            Double bottleCost = .10;
+
+            //input
+            for (int day = 1; day < 8; day++)
+            {
+
+                do
+                {
+                    Console.WriteLine("Enter the total bottles for day " + day);
+                    validateEntry = Console.ReadLine();
+                    IsString = int.TryParse(validateEntry, out dailyTotBottles);
+                    if (!(IsString))
+                    {
+                        Console.WriteLine("You entered a non numeric number, Please try again");
+                    }
+
+                } while (!(IsString));
+
+                //process
+                wklytotBottles = dailyTotBottles + wklytotBottles;
+            }
+
+            //process
+
+            // TODO: Refactor without magic numbers
+            wklytotPaid = wklytotBottles * bottleCost;
+            //output
+
+            Console.WriteLine("The total number bottles for the week is " + wklytotBottles + " the ammont paid is " + wklytotPaid + " dollars");
+            Console.ReadLine();
         }
     }
 }
-
